37a64858820ac17741e8e03d0194858413fc7972
Tarcisio <tarcisiodavicavalcante@gmail.com>
2026-09-20 18:43:56 -0300

    não sei

--- principal.js
+++ principal.js
@@ -1,6 +1,11 @@
 const filtro = document.getElementById("filtro");
 const select = document.getElementById("select-filtro");
 
-select.addEventListener('change', () => {
-  filtro.textContent = select.options[select.selectedIndex].text;
+if (filtro && select) {
+    filtro.addEventListener('click', () => {
+    select.showPicker();
 });
+
+    select.addEventListener('change', () => {
+    filtro.textContent = select.options[select.selectedIndex].text;});
+}
