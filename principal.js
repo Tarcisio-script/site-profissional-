fc39833740b02502ac411b2edcd106b619c1aab5
Tarcisio <tarcisiodavicavalcante@gmail.com>
2026-09-22 17:35:35 -0300

    id e classes adicionadas

--- principal.js
+++ principal.js
@@ -5,14 +5,14 @@ select.addEventListener("change", function () {
     const cards = grade.querySelectorAll(".produto-card");
 
     if (this.value === "opcao1") {
-        // esconde todos os cards da grade
+    
         cards.forEach((card) => (card.style.display = "none"));
     } else if (this.value === "opcao2") {
-        // espaço reservado pra próxima lógica
+
     } else if (this.value === "opcao3") {
-        // espaço reservado pra próxima lógica
+
     } else {
-        // valor "Filtro" (padrão) -> mostra tudo de novo
+
         cards.forEach((card) => (card.style.display = ""));
     }
 });
